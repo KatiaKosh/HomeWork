9ad5e8435a38515830267bbf53aa9ebea635b803
katia <katia00712@mail.ru>
2022-09-19 22:01:28 +0500

    HomeWork1

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача №6
+Console.WriteLine("Введите число:");
+int num_1 = Convert.ToInt32(Console.ReadLine());
+
+if (num_1 % 2 == 1)
+{
+Console.WriteLine("Число " + num_1 + " является: Нечётным");
+}
+else
+{
+Console.WriteLine("Число " + num_1 + "является: Чётным");
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача №8
+
+int n = 1;
+Console.Write("Введите число: ");
+int m = int.Parse(Console.ReadLine());
+for (int i = n; i <= m; i++)
+{
+if (i % 2 == 0) 
+{
+Console.Write(i + " ");
+}
+}
+
+    
